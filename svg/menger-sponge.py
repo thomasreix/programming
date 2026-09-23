97a2633418ae1bb4b003af16ee6533ffbd0ff020
thomasreix <beglary@gentoo.home>
2026-09-23 17:57:19 +0200

    update

--- svg/menger-sponge.py
+++ svg/menger-sponge.py
@@ -1,3 +1,8 @@
+UP = 0
+LEFT = 1
+RIGHT = 2
+
+
 def generate_face(iteration):
     width = 3**iteration
     face_points = [(0, 0), (width, 0), (width, 1), (1, 1)]
@@ -17,19 +22,13 @@ def generate_face(iteration):
     return face_points
 
 
-def map_points(points, offset_x, offset_y, size, angle_x, angle_y):
-    mapped_points = []
-    for point in points:
-        point_x, point_y = point
-        mapped_point_x = point_x * size + offset_x + (point_y * angle_x)
-        mapped_point_y = point_y * size + offset_y + (point_x * angle_y)
-        mapped_point = (mapped_point_x, mapped_point_y)
-        mapped_points.append(mapped_point)
-
-    return mapped_points
+def draw_face(face_points, face_x, face_y, size, ratio, side, colors):
+    if side is LEFT:
+        for point in face_points:
+            
 
 
-def draw_face(points, color, stroke_color, stroke_width):
+
     points_string = ""
 
     for point in points:
@@ -40,8 +39,7 @@ def draw_face(points, color, stroke_color, stroke_width):
     face = f"""<polygon
         points="{points_string}"
         fill="{color}"
-        stroke="{stroke_color}"
-        stroke-width="{stroke_width}"/>
+        stroke-width="0"/>
 """
 
     return face
@@ -51,8 +49,7 @@ def generate_svg(poligons):
     svg = f"""<svg xmlns="http://www.w3.org/2000/svg"
     width="800"
     height="600"
-    viewBox="0 0 800 600"
-    style="background-color: #0f0f0f;">
+    viewBox="0 0 800 600">
 
 """
 
@@ -65,25 +62,21 @@ def generate_svg(poligons):
 
 
 def main():
-    up_color = "#f7f7ff"
-    left_color = "#c8c7ff"
-    right_color = "#9491ff"
-
-    stroke_color = "#625fd7"
-    stroke_width = 2
+    up_color = "#ff0000"
+    left_color = "#00ff00"
+    right_color = "#0000ff"
+    colors = [up_color, left_color, right_color]
 
     iteration = 1
+    face_x, face_y = 100, 100
+    size = 50
+    ratio = [3, 4]
+    side = LEFT
 
     face_points = generate_face(iteration)
+    left_face = draw_face(face_points, face_x, face_y, size, ratio, side, colors)
 
-    face_x, face_y = 200, 200
-    size = 100
-    angle_x, angle_y = 2, 30
-    mapped_points = map_points(face_points, face_x, face_y, size, angle_x, angle_y)
-
-    face = draw_face(mapped_points, left_color, stroke_color, stroke_width)
-
-    poligons = [face]
+    poligons = [left_face]
     generate_svg(poligons)
 
 
